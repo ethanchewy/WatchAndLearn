cdb8d7a878fa5fb7128d20b4630511c414e6659e
Bhavya Shah <bhavyashah@v1020-wn-14-68.campus-dynamic.uwaterloo.ca>
2016-09-17 10:40:19 -0400

    added frontend + search functionality

---
+++ js/view.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+    var videoId = parent.document.URL.substring(parent.document.URL.indexOf('?videoId=') + 9, parent.document.URL.length);
+    var videoHTML = getVideoHTML(videoId);
+    calcWidth();
+    $("#video").append(videoHTML);
+    queryDictionary('date');
+});
+
+function getVideoHTML(videoId) {
+    var html = "<iframe title='YouTube video player' class='youtube-player' height='100%'id='preview-frame' src='http://www.youtube.com/embed/" + videoId + "'allowFullScreen></iframe>"
+    return html;
+}
+
+
+var calcWidth = function() {
+    $('#preview-frame').width($('#video').width());
+}
+$(window).resize(function() {
+    calcWidth();
+}).load(function() {
+    calcWidth();
+});
+
+function queryDictionary(word){
+  var xmlhttp = new XMLHttpRequest();
+  var url = "https://crossorigin.me/http://www.dictionaryapi.com/api/v1/references/learners/xml/"+word+"?key=eaf69752-c354-4489-8cc6-99948b85a285";
+
+ xmlhttp.onreadystatechange = function() {
+     if (this.readyState == 4 && this.status == 200) {
+         var x2js = new X2JS();
+         var jsonObj = x2js.xml_str2json( this.responseText );
+        //  console.warn(jsonObj.entry_list.entry);
+         if(jsonObj.entry_list.entry)
+         var audioFile = jsonObj.entry_list.entry.sound?jsonObj.entry_list.entry.sound.wav:jsonObj.entry_list.entry[0].sound.wav;
+         var subDirectory = "";
+         if(audioFile.indexOf('bix') == 0){
+           subDirectory = "bix";
+         }else if (audioFile.indexOf('gg') == 0){
+           subDirectory = "gg";
+         }else if (/^\d\w/g.exec(audioFile) != null){
+           subDirectory="number";
+         }else{
+           subDirectory = audioFile[0];
+         }
+         var path =  "http://media.merriam-webster.com/soundc11/"+subDirectory.toLowerCase()+"/"+audioFile;
+      var audioElement = document.createElement('audio');
+      audioElement.setAttribute('src', path);
+      audioElement.play();
+     // console.log(this.responseText);
+     }
+ };
+ xmlhttp.open("GET", url, true);
+ xmlhttp.send();
+}
