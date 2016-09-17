a425440a58e474b927bd6450f7cee1175290a2c1
ethanchewy <17chiue@gmail.com>
2016-09-17 03:05:57 -0400

    fix

--- backend/Captions/js/javascript.js
+++ backend/Captions/js/javascript.js
@@ -1,11 +1,16 @@
 function reqListener () {
-	var x2js = new X2JS();
-	var jsonObj = x2js.xml_str2json( this.responseText );
+	x2js = new X2JS();
+	jsonObj = x2js.xml_str2json( this.responseText );
+	//filtered_data = JSON.parse(jsonObj);
   	console.log(this.responseText);
   	console.log(jsonObj);
 }
+var x2js,
+	jsonObj,
+	filtered_data;
 var oReq = new XMLHttpRequest();
-var url = "https://video.google.com/timedtext?lang=en&v=" + searched_url;
+//var url = "https://video.google.com/timedtext?lang=en&v=" + searched_url;
 oReq.addEventListener("load", reqListener);
-oReq.open("GET", url);
+oReq.open("GET", "https://video.google.com/timedtext?lang=en&v=apbSsILLh28");
 oReq.send();
+//var filtered_data = JSON.parse(jsonObj);
